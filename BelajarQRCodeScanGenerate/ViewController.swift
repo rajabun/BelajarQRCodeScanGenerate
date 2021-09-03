e63e7e6110ec574b1e9d30f2a6dfe7e5d31708ed
Muhammad Rajab <ichijo_rajab@hotmail.com>
2021-09-03 07:34:36 +0700

    Initial Commit

---
+++ BelajarQRCodeScanGenerate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BelajarQRCodeScanGenerate
+//
+//  Created by Muhammad Rajab Priharsanto on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
